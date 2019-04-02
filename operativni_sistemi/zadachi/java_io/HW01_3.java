d0b41e72a21bcc8aed813ed6c93ab770f8c5c3bb
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2019-04-02 19:51:43 +0200

    Update HW01_3.java

--- operativni_sistemi/zadachi/java_io/HW01_3.java
+++ operativni_sistemi/zadachi/java_io/HW01_3.java
@@ -1,66 +1,60 @@
-//package Lab_2019;
+//package lab123_practice;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class HW01_3 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 
-		String absPath = "C:/Users/Teo Grozdanoski/Documents/myFolder/";
-		String source = absPath + "izvorbuf.txt";
-		String destination = absPath + "destinacijabuf.txt";
-		
-		File fsrc = null;
-		File fdst = null;
-		
+		String absolutePath = "C:\\Users\\Teo Grozdanoski\\Documents\\FINKI\\Leten_Semestar_2018-2019\\Operativni_Sistemi\\Kod\\io_test\\";
+		String stringSource = absolutePath + "izvor.txt";
+		String stringDestination = absolutePath + "destinacija.txt";
+
+		File src = null;
+		File dst = null;
+
 		BufferedReader reader = null;
 		BufferedWriter writer = null;
-		
-		String line = "";
-		String outputString = "";
-		
-		StringBuilder sb = new StringBuilder();
-		
-		try {		
-			
-			fsrc = new File(source);
-			fdst = new File(destination);
-			
-			reader = new BufferedReader(new InputStreamReader(new FileInputStream(fsrc)));
-			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fdst)));
-			
-			if(!fsrc.exists()) {
-				System.out.println("Izvornata datoteka ne postoi");
-				return;
-			}
-			if(!fdst.exists()) {
-				System.out.println("Destinaciskata datoteka ne postoi");
-				return;
-			}
-			
-			while((line = reader.readLine()) != null) {
+
+		StringBuilder sb = null;
+
+		try {
+
+			src = new File(stringSource);
+			dst = new File(stringDestination);
+
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(src)));
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dst)));
+
+			sb = new StringBuilder();
+			String line = "";
+
+			while ((line = reader.readLine()) != null) {
 				sb.append(line + "\n");
 			}
-			
-			sb.reverse();
-			outputString = sb.toString();
-			
-			writer.write(outputString);
-			
-		} catch (Exception e) {
-			throw e;
+
+			writer.write(sb.reverse().toString());
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+
 		} finally {
+
 			reader.close();
 			writer.flush();
 			writer.close();
+
 		}
-		
+
 	}
 
 }
